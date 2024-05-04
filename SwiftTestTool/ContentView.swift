5c1e803d8c902ce5a52f8226c6fd7178d3186e40
HeonJin Ha <weizen511@gmail.com>
2024-05-04 21:45:10 +0900

    Initial Commit

---
+++ SwiftTestTool/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftTestTool
+//
+//  Created by HeonJin Ha on 5/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftTestTool/SwiftTestToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTestToolApp.swift
+//  SwiftTestTool
+//
+//  Created by HeonJin Ha on 5/4/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTestToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
